18a9235a1e58b02d833752746165285878437af5
TwAnjos <thiagowaltrick@gmail.com>
2021-02-21 14:03:48 -0300

    Starting Project

---
+++ src/main/java/com/tw/TesteSpringMavem/TesteSpringMavemApplication.java
@@ -0,0 +1,13 @@
+package com.tw.TesteSpringMavem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringMavemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringMavemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/TesteSpringMavem/TesteSpringMavemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.TesteSpringMavem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringMavemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
